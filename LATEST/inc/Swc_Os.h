35d0d6097b85c056ce545ded4de5e89cfb106dcf
root <nagaraja.hm.0001@gmail.com>
2021-12-13 18:04:27 +0100

    Update: submodule Swc_Os

---
+++ LATEST/inc/Swc_Os.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_Swc_Os{
+   public:
+      void StartupHook(void);
+};
+
+extern class_Swc_Os Swc_Os;
+
---
+++ LATEST/src/Swc_Os.cpp
@@ -0,0 +1,7 @@
+#include "Swc_Os.h"
+
+class_Swc_Os Swc_Os;
+
+void class_Swc_Os::StartupHook(void){
+}
+
