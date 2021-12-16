df2f465fc71209c8e6c74698a0cc02551e298277
root <nagaraja.hm.0001@gmail.com>
2021-12-16 10:43:17 +0100

    Update: submodule Swc_Os

--- LATEST/inc/Swc_Os.h
+++ LATEST/inc/Swc_Os.h
@@ -4,6 +4,7 @@ class class_Swc_Os{
    public:
       void StartupHook  (void);
       void ShutdownHook (void);
+      void TASK_Idle    (void);
 };
 
 extern class_Swc_Os Swc_Os;
--- LATEST/src/Swc_Os.cpp
+++ LATEST/src/Swc_Os.cpp
@@ -1,10 +1,16 @@
 #include "Swc_Os.h"
 
+#include "EcuM.h"
+
 class_Swc_Os Swc_Os;
 
 void class_Swc_Os::StartupHook(void){
 }
 
 void class_Swc_Os::ShutdownHook(void){
+}
+
+void class_Swc_Os::TASK_Idle(void){
+   EcuM.MainFunction();
 }
 
