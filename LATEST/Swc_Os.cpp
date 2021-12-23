fda224054d37e3f75e03a9da87f8d82222258b9f
root <nagaraja.hm.0001@gmail.com>
2021-12-23 23:25:51 +0100

    Update: submodule Swc_Os

--- LATEST/Swc_Os.cpp
+++ LATEST/Swc_Os.cpp
@@ -1,6 +1,7 @@
 #include "Swc_Os.h"
 
 #include "EcuM.h"
+#include "NvM.h"
 
 class_Swc_Os Swc_Os;
 
@@ -12,5 +13,6 @@ void class_Swc_Os::ShutdownHook(void){
 
 void class_Swc_Os::TASK_Idle(void){
    EcuM.MainFunction();
+   NvM.MainFunction();
 }
 
