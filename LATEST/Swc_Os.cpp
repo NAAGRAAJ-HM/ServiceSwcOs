dc701bb15ffaafd0ccd4904dc236a2b20ee47cad
root <nagaraja.hm.0001@gmail.com>
2022-01-12 10:25:58 +0100

    Update: submodule Swc_Os

--- LATEST/Swc_Os.cpp
+++ LATEST/Swc_Os.cpp
@@ -5,13 +5,13 @@
 
 class_Swc_Os Swc_Os;
 
-void class_Swc_Os::StartupHook(void){
+FUNC(void, SWC_OS_CODE) class_Swc_Os::StartupHook(void){
 }
 
-void class_Swc_Os::ShutdownHook(void){
+FUNC(void, SWC_OS_CODE) class_Swc_Os::ShutdownHook(void){
 }
 
-void class_Swc_Os::TASK_Idle(void){
+FUNC(void, SWC_OS_CODE) class_Swc_Os::TASK_Idle(void){
    EcuM.MainFunction();
    NvM.MainFunction();
 }
--- LATEST/Swc_Os.h
+++ LATEST/Swc_Os.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "Std_Types.h"
+#include "Compiler_Cfg_Swc_Os.h"
 
 class class_Swc_Os{
    public:
-      void StartupHook  (void);
-      void ShutdownHook (void);
-      void TASK_Idle    (void);
+      FUNC(void, SWC_OS_CODE) StartupHook  (void);
+      FUNC(void, SWC_OS_CODE) ShutdownHook (void);
+      FUNC(void, SWC_OS_CODE) TASK_Idle    (void);
 };
 
 extern class_Swc_Os Swc_Os;
