f18920ffa237f6b83f59c2cf6c302899ce283472
root <nagaraja.hm.0001@gmail.com>
2022-02-16 11:55:24 +0100

    Update: submodule Swc_Os

--- LATEST/Swc_Os.h
+++ LATEST/Swc_Os.h
@@ -1,18 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Swc_Os.cpp                               */
+/* File   : Swc_Os.h                                 */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "Swc_Os.h"
-
-#include "Swc_Os_Unused.h"
-
-#include "NvM_SchM.h"
-#include "EcuM_SchM.h"
+#include "Compiler_Cfg_Os.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -25,17 +20,14 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Swc_Os:
-      public class_module
-   ,  public interface_Swc_Os
-{
+class interface_Swc_Os{
    public:
-      FUNC(void, OS_CODE) InitFunction   (void);
-      FUNC(void, OS_CODE) DeInitFunction (void);
-      FUNC(void, OS_CODE) MainFunction   (void);
-      FUNC(void, OS_CODE) StartupHook    (void);
-      FUNC(void, OS_CODE) ShutdownHook   (void);
-      FUNC(void, OS_CODE) TASK_Idle      (void);
+/*****************************************************/
+/* FUNCTIONS                                         */
+/*****************************************************/
+      FUNC(void, OS_CODE) StartupHook  (void);
+      FUNC(void, OS_CODE) ShutdownHook (void);
+      FUNC(void, OS_CODE) TASK_Idle    (void);
 };
 
 /*****************************************************/
@@ -49,30 +41,7 @@ class module_Swc_Os:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Swc_Os Swc_Os;
-
-/*****************************************************/
-/* FUNCTIONS                                         */
-/*****************************************************/
-FUNC(void, OS_CODE) module_Swc_Os::InitFunction(void){
-}
-
-FUNC(void, OS_CODE) module_Swc_Os::DeInitFunction(void){
-}
-
-FUNC(void, OS_CODE) module_Swc_Os::MainFunction(void){
-}
-
-FUNC(void, OS_CODE) module_Swc_Os::StartupHook(void){
-}
-
-FUNC(void, OS_CODE) module_Swc_Os::ShutdownHook(void){
-}
-
-FUNC(void, OS_CODE) module_Swc_Os::TASK_Idle(void){
-   SchM_Client_ptr_EcuM->MainFunction();
-   SchM_Client_ptr_NvM->MainFunction();
-}
+extern interface_Swc_Os *Os_Client_ptr_Swc_Os;
 
 /*****************************************************/
 /* EOF                                               */
