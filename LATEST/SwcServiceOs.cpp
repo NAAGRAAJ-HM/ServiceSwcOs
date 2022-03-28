b0e37231891d82a613fc4d83b0be6d931702043a
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:48:52 +0200

    Update: AUTOSAR service software component SwcServiceOs

--- LATEST/SwcServiceOs.cpp
+++ LATEST/SwcServiceOs.cpp
@@ -38,13 +38,13 @@ class module_SwcServiceOs:
    ,  public infSwcServiceOs_Os
 {
    public:
-      FUNC(void, OS_CODE) InitFunction   (void);
-      FUNC(void, OS_CODE) DeInitFunction (void);
-      FUNC(void, OS_CODE) GetVersionInfo (void);
-      FUNC(void, OS_CODE) MainFunction   (void);
-      FUNC(void, OS_CODE) StartupHook    (void);
-      FUNC(void, OS_CODE) ShutdownHook   (void);
-      FUNC(void, OS_CODE) TASK_Idle      (void);
+      FUNC(void, SWCSERVICEOS_CODE) InitFunction   (void);
+      FUNC(void, SWCSERVICEOS_CODE) DeInitFunction (void);
+      FUNC(void, SWCSERVICEOS_CODE) GetVersionInfo (void);
+      FUNC(void, SWCSERVICEOS_CODE) MainFunction   (void);
+      FUNC(void, SWCSERVICEOS_CODE) StartupHook    (void);
+      FUNC(void, SWCSERVICEOS_CODE) ShutdownHook   (void);
+      FUNC(void, SWCSERVICEOS_CODE) TASK_Idle      (void);
 
    private:
       CONST(Std_TypeVersionInfo, SWCSERVICEOS_CONST) VersionInfo = {
@@ -72,20 +72,20 @@ VAR(module_SwcServiceOs, SWCSERVICEOS_VAR) SwcServiceOs;
 CONSTP2VAR(infEcuMClient, SWCSERVICEOS_VAR, SWCSERVICEOS_CONST) gptrinfEcuMClient_SwcServiceOs = &SwcServiceOs;
 CONSTP2VAR(infDcmClient,  SWCSERVICEOS_VAR, SWCSERVICEOS_CONST) gptrinfDcmClient_SwcServiceOs  = &SwcServiceOs;
 CONSTP2VAR(infSchMClient, SWCSERVICEOS_VAR, SWCSERVICEOS_CONST) gptrinfSchMClient_SwcServiceOs = &SwcServiceOs;
-infSwcServiceOs_Os* gptrinfSwcServiceOs_Os;
+CONSTP2VAR(infSwcServiceOs_Os, SWCSERVICEOS_VAR, SWCSERVICEOS_CONST) gptrinfSwcServiceOs_Os    = &SwcServiceOs;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
-FUNC(void, OS_CODE) module_SwcServiceOs::InitFunction(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::InitFunction(void){
    SwcServiceOs.IsInitDone = E_OK;
 }
 
-FUNC(void, OS_CODE) module_SwcServiceOs::DeInitFunction(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::DeInitFunction(void){
    SwcServiceOs.IsInitDone = E_NOT_OK;
 }
 
-FUNC(void, OS_CODE) module_SwcServiceOs::GetVersionInfo(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::GetVersionInfo(void){
 #if(STD_ON == SwcServiceOs_DevErrorDetect)
 //TBD: API parameter check
    Det_ReportError(
@@ -93,24 +93,29 @@ FUNC(void, OS_CODE) module_SwcServiceOs::GetVersionInfo(void){
 #endif
 }
 
-FUNC(void, OS_CODE) module_SwcServiceOs::MainFunction(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::MainFunction(void){
 }
 
-FUNC(void, OS_CODE) module_SwcServiceOs::StartupHook(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::StartupHook(void){
 }
 
-FUNC(void, OS_CODE) module_SwcServiceOs::ShutdownHook(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::ShutdownHook(void){
 }
 
 #include "infNvM_SchM.hpp"
 #include "infEcuM_SchM.hpp"
 
-FUNC(void, OS_CODE) module_SwcServiceOs::TASK_Idle(void){
+FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::TASK_Idle(void){
    gptrinfSchMClient_EcuM->MainFunction();
    gptrinfSchMClient_NvM->MainFunction();
 }
 
-#include "SwcServiceOs_Unused.hpp"
+class class_SwcServiceOs_Unused{
+   public:
+      FUNC(void, SWCSERVICEOS_CODE) StartupHook  (void);
+      FUNC(void, SWCSERVICEOS_CODE) ShutdownHook (void);
+      FUNC(void, SWCSERVICEOS_CODE) TASK_Idle    (void);
+};
 
 /******************************************************************************/
 /* EOF                                                                        */
