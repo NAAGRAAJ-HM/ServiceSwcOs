b9e6d272d107156f9a6c51e77a5630dd4117e381
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-03 10:10:09 +0000

    Update: Service software component for OS

--- LATEST/SwcServiceOs.cpp
+++ LATEST/SwcServiceOs.cpp
@@ -85,79 +85,79 @@ FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, SWCSERVICEOS_CONFIG_DATA, SWCSERVICEOS_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == SwcServiceOs_InitCheck)
-   if(E_OK == IsInitDone){
-#if(STD_ON == SwcServiceOs_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK != IsInitDone){
 #endif
-   }
-   else{
-#endif
-      if(NULL_PTR == lptrCfgModule){
-#if(STD_ON == SwcServiceOs_DevErrorDetect)
-         Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-         );
-#endif
-      }
-      else{
+      if(NULL_PTR != lptrCfgModule){
          if(STD_LOW){
-// check lptrCfgModule for memory faults
+            // check lptrCfgModule for memory faults
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfgCanIf as back-up configuration
+            // use PBcfgCanIf as back-up configuration
 //            lptrCfg = &PBcfgSwcServiceOs;
          }
       }
-      IsInitDone = E_OK;
+      else{
+#if(STD_ON == SwcServiceOs_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
 #if(STD_ON == SwcServiceOs_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == SwcServiceOs_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
 
 FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::DeInitFunction(void){
 #if(STD_ON == SwcServiceOs_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == SwcServiceOs_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK == IsInitDone){
 #endif
+#if(STD_ON == SwcServiceOs_InitCheck)
+      IsInitDone = E_NOT_OK;
    }
    else{
+#if(STD_ON == SwcServiceOs_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
 #endif
-      IsInitDone = E_NOT_OK;
-#if(STD_ON == SwcServiceOs_InitCheck)
    }
 #endif
 }
 
 FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::MainFunction(void){
 #if(STD_ON == SwcServiceOs_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == SwcServiceOs_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
-#endif
-   }
-   else{
+   if(E_OK == IsInitDone){
 #endif
 #if(STD_ON == SwcServiceOs_InitCheck)
+   }
+   else{
+#if(STD_ON == SwcServiceOs_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
