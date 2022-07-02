8fae29638d66f757a4c10e73ec03561cb28e571c
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-02 11:42:51 +0000

    Update: Service software component for for safe operating system

--- LATEST/SwcServiceOs.cpp
+++ LATEST/SwcServiceOs.cpp
@@ -83,12 +83,7 @@ FUNC(void, SWCSERVICEOS_CODE) module_SwcServiceOs::InitFunction(
    ){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_HIGH){
-            lptrCfg = lptrCfgModule;
-         }
-         else{
-            lptrCfg = &PBcfgSwcServiceOs;
-         }
+         lptrCfg = lptrCfgModule;
       }
       else{
 #if(STD_ON == SwcServiceOs_DevErrorDetect)
