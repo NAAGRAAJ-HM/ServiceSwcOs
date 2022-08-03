04619582b14739e03f52215a0dc7a7c2eb068e66
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-03 17:37:47 +0200

    Update: Service software component for for safe operating system

---
+++ BldcFocSensorless_LATEST/SwcServiceOs_core.hpp
@@ -0,0 +1,66 @@
+#pragma once
+/******************************************************************************/
+/* File   : SwcServiceOs_core.hpp                                             */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_SwcServiceOs.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SWCSERVICEOS_CORE_FUNCTIONALITIES                                      \
+              FUNC(void, SWCSERVICEOS_CODE) StartupHook  (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) ShutdownHook (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) TASK_Idle    (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) TASK_1ms     (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) TASK_5ms     (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) TASK_10ms    (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) TASK_20ms    (void);               \
+              FUNC(void, SWCSERVICEOS_CODE) TASK_25ms    (void);               \
+
+#define SWCSERVICEOS_CORE_FUNCTIONALITIES_VIRTUAL                              \
+      virtual FUNC(void, SWCSERVICEOS_CODE) StartupHook  (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) ShutdownHook (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) TASK_Idle    (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) TASK_1ms     (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) TASK_5ms     (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) TASK_10ms    (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) TASK_20ms    (void) = 0;           \
+      virtual FUNC(void, SWCSERVICEOS_CODE) TASK_25ms    (void) = 0;           \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_SwcServiceOs_Functionality{
+   public:
+      SWCSERVICEOS_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
